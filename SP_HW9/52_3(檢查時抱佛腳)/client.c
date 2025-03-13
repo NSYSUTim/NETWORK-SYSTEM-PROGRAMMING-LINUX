1cbee1423fff00fe7ba9c23250d6806ab7f4c40b
Tim <0402tim@gmail.com>
2025-03-13 14:05:25 +0800

    first commit

--- SP_HW9/52_3(檢查時抱佛腳)/client.c
+++ SP_HW9/52_3(檢查時抱佛腳)/client.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* 
  * client program of 52_3
  */
@@ -95,70 +94,4 @@ int main(int argc, char *argv[]){
 
 	exit(EXIT_SUCCESS);
 
-=======
-#include "seqnum.h"
-
-#define MAX_MSG_SIZE 1024
-
-void errExit(const char *err) {
-    perror(err);
-    exit(EXIT_FAILURE);
-}
-
-int main(int argc, char *argv[]) {
-    mqd_t serverQueue, clientQueue;
-    char clientQueueName[128];
-    struct mq_attr attr;
-    struct request req;
-    struct response resp;
-
-    if (argc > 1 && strcmp(argv[1], "--help") == 0) {
-        printf("%s [seq-len]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    // 設定訊息佇列屬性
-    attr.mq_flags = 0;               // 阻塞模式
-    attr.mq_maxmsg = 10;             // 最大訊息數
-    attr.mq_msgsize = MAX_MSG_SIZE;  // 每則訊息的最大大小
-    attr.mq_curmsgs = 0;             // 當前訊息數
-
-    // 創建客戶端的 POSIX 訊息佇列
-    snprintf(clientQueueName, sizeof(clientQueueName), "/seqnum_cl.%ld", (long)getpid());
-    if ((clientQueue = mq_open(clientQueueName, O_CREAT | O_RDONLY, S_IRUSR | S_IWUSR, &attr)) == (mqd_t)-1) {
-        errExit("mq_open client");
-    }
-
-    // 打開伺服器的 POSIX 訊息佇列
-    if ((serverQueue = mq_open(SERVER_QUEUE_NAME, O_WRONLY)) == (mqd_t)-1) {
-        errExit("mq_open server");
-    }
-
-    // 發送請求到server
-    req.pid = getpid();
-    // req.seqLen = (argc > 1) ? atoi(argv[1]) : 1;  // 沒給 seq-len 就設 1
-
-    int fd;
-    if( (fd=open(argv[1],O_RDONLY,0)) ==-1){
-        errExit("server open");
-    }
-    int filsiz;
-    filsiz = lseek(fd,0,SEEK_END);
-    req.seqLen = filsiz;
-
-    if (mq_send(serverQueue, (char *)&req, sizeof(req), 0) == -1) {
-        errExit("mq_send");
-    }
-
-    // 接收伺服器的回應
-    if (mq_receive(clientQueue, (char *)&resp, MAX_MSG_SIZE, NULL) == -1) {
-        errExit("mq_receive");
-    }
-
-    printf("Received seq_num: %d\n", resp.seqNum);
-
-    mq_close(clientQueue);
-    mq_unlink(clientQueueName);
-    return 0;
->>>>>>> 65e707a (提交專案)
 }
--- SP_HW9/52_3(檢查時抱佛腳)/server.c
+++ SP_HW9/52_3(檢查時抱佛腳)/server.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* 
  * server program of 52_3
  */
@@ -109,65 +108,4 @@ int main(int argc, char *argv[]){
 
 	exit(EXIT_SUCCESS);
 
-=======
-#include "seqnum.h"
-
-#define MAX_MSG_SIZE 1024
-
-void errExit(const char *err) {
-    perror(err);
-    exit(EXIT_FAILURE);
-}
-
-// server發送當前seqnum，並更新加上client傳來的seqnum
-int main() {
-    mqd_t serverQueue;
-    char clientQueueName[128];
-    struct mq_attr attr;
-    struct request req;
-    struct response resp;
-    int seqNum = 0;
-
-    // 設定訊息佇列屬性
-    attr.mq_flags = 0;               // 阻塞模式
-    attr.mq_maxmsg = 10;             // 最大訊息數
-    attr.mq_msgsize = MAX_MSG_SIZE;  // 每則訊息的最大大小
-    attr.mq_curmsgs = 0;             // 當前訊息數
-
-    // 創建伺服器的 POSIX 訊息佇列
-    if ((serverQueue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_RDONLY, S_IRUSR | S_IWUSR, &attr)) == (mqd_t)-1) {
-        errExit("mq_open");
-    }
-
-    for (;;) {
-        // 從伺服器佇列中接收請求
-        if (mq_receive(serverQueue, (char *)&req, MAX_MSG_SIZE, NULL) == -1) {
-            errExit("mq_receive");
-        }
-
-        // 客戶端訊息佇列名稱
-        snprintf(clientQueueName, sizeof(clientQueueName), "/seqnum_cl.%ld", (long)req.pid);
-
-        // 回應client
-        resp.seqNum = seqNum;
-        seqNum += req.seqLen;
-
-        // 發送回應到客戶端的 POSIX 訊息佇列
-        mqd_t clientQueue;
-        if ((clientQueue = mq_open(clientQueueName, O_WRONLY)) == (mqd_t)-1) {
-            fprintf(stderr, "Error opening client queue: %s\n", clientQueueName);
-            continue;
-        }
-
-        if (mq_send(clientQueue, (char *)&resp, sizeof(resp), 0) == -1) {
-            fprintf(stderr, "Error sending response to client\n");
-        }
-
-        mq_close(clientQueue);
-    }
-
-    mq_close(serverQueue);
-    mq_unlink(SERVER_QUEUE_NAME);
-    return 0;
->>>>>>> 65e707a (提交專案)
 }
